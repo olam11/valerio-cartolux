bd8c6cea5ae145fab83a145ad35c6d08603db960
olam11 <malo.gryspeerdt@gmail.com>
2024-06-14 19:18:11 +0200

    v1 streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title='CARTOLUX',
-    page_icon=':map:',
+    page_icon=':world_map:',
 )
+st.title("CARTOLUX")
 st.write("Ceci est un site web en cours de développement pour Valerio Delsart Moretti")
