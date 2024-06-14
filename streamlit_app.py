21acbe81013c822f2c793380b017935848bc5f0a
olam11 <malo.gryspeerdt@gmail.com>
2024-06-14 20:11:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ st.set_page_config(
 )
 st.title("CARTOLUX")
 st.write("Ceci est un site web en cours de développement pour Valerio Delsart Moretti")
+st.write("exemple : ")
 image = Image.open("Carte_de_France_de_Mathias_Robert_de_Hesseln_de_1780_(haute_résolution).jpg")
 st.image(image)
