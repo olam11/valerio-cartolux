d923ed343c1e9c68b3b06e6cf3633ec4fb35c2bb
olam11 <malo.gryspeerdt@gmail.com>
2024-06-14 19:46:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import Pgmagick
 
 st.set_page_config(
     page_title='CARTOLUX',
@@ -6,4 +7,5 @@ st.set_page_config(
 )
 st.title("CARTOLUX")
 st.write("Ceci est un site web en cours de développement pour Valerio Delsart Moretti")
-st.image("Carte_de_France_de_Mathias_Robert_de_Hesseln_de_1780_(haute_résolution).jpg")
+image = Pgmagick.Image.open("Carte_de_France_de_Mathias_Robert_de_Hesseln_de_1780_(haute_résolution).jpg")
+st.image(image)
