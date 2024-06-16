7580477306c4dbed0deb449e766b3297daee33b1
olam11 <malo.gryspeerdt@gmail.com>
2024-06-16 20:16:43 +0200

    ajout du texte

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ st.set_page_config(
 )
 st.title("CARTOLUX")
 st.write("Ceci est un site web en cours de développement pour Valerio Delsart Moretti")
-st.write("exemple : ")
-image = Image.open("Carte_de_France_de_Mathias_Robert_de_Hesseln_de_1780_(haute_résolution).jpg")
-st.image(image)
+st.write("Vous avez plus d'un à essayer de dessiner une carte pendant longtemps...en vain. Mais aussi à en rêver pour décorer un espace, agrémenter vos œuvres littéraires etc.
+Ne perdez plus votre temps!
+Dès aujourd'hui, faites confiance à CARTOLUX !!")
+#image = Image.open("Carte_de_France_de_Mathias_Robert_de_Hesseln_de_1780_(haute_résolution).jpg")
+#st.image(image)
