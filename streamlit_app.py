e93e34010122e81a11899f1ec1f41b7a695fc2fd
olam11 <malo.gryspeerdt@gmail.com>
2024-06-17 18:19:21 +0200

    ajout d'un about

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ from PIL import Image
 st.set_page_config(
     page_title='CARTOLUX',
     page_icon=':world_map:',
-)
+    menu_items= {"about":"Ceci est un site web en cours de développement"},
+    )
 st.title("CARTOLUX")
-st.write("Ceci est un site web en cours de développement pour Valerio Delsart Moretti")
-st.write("Vous avez plus d'un à essayer de dessiner une carte pendant longtemps...en vain. Mais aussi à en rêver pour décorer un espace, agrémenter vos œuvres littéraires etc. Ne perdez plus votre temps! Dès aujourd'hui, faites confiance à CARTOLUX")
+st.write("Vous avez plus d'un à essayer de dessiner une carte pendant longtemps...en vain. Mais aussi à en rêver pour décorer un espace, agrémenter vos œuvres littéraires etc. Ne perdez plus votre temps! Dès aujourd'hui, faites confiance à CARTOLUX !")
 #image = Image.open("Carte_de_France_de_Mathias_Robert_de_Hesseln_de_1780_(haute_résolution).jpg")
 #st.image(image)
