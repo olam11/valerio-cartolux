34f25fb53a8176455531a539799dcca423361ebf
olam11 <malo.gryspeerdt@gmail.com>
2024-07-08 19:56:41 +0200

    ajout des images sur le site

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,44 +8,81 @@ st.set_page_config(
     layout='wide'
     )
 
+def afficher_image(nom_image):
+    image = Image.open(nom_image)
+    st.image(image)
+
 st.title("CARTOLUX")
-st.write("""
-###### ***Des carte comme vous les souhaitez !***\n         
+intro = """
+###### ***Des carte comme vous les souhaitez !***\n  
+---       
 ****Vous avez plus d'une fois essayé de dessiner une carte, pendant longtemps...en vain. Mais aussi en rêver pour décorer un espace, agrémenter vos œuvres littéraires etc.       
 Ne perdez plus votre temps! Dès aujourd'hui, faites confiance à CARTOLUX !****\n
+---
 :red[Admirez] la délicatesse d'un trait précis et net retraçant la carte de votre choix.             
 :red[Exaltez-vous] face au réalisme et à la beauté des œuvres que vous commanderez.      
 :red[Réjouissez-vous] de la beauté de nos cartes pour leur prix incroyablement bas.       
 :red[Soyez surpris] par les multiples possibilités d'une carte paramétrable.        
 :red[Ayez confiant] en vos cartographes polyvalents maîtrisant tous les types de carte.        
 :red[Jouissez] de nos nombreux styles et formats que vous offrent la carte qui vous convient le mieux.\n
+---
 ### STYLES
 Nos nombreux styles vous permettront de créer votre carte sur-mesure.      
 Grâce à nos options, personnalisations et suppléments, ordonnez votre carte!   
 P.S. : en cas de besoin, contactez-nous!\n\n
-
-1) ##### **Fantasy**  
+---
+"""
+fantasy = """1) ##### **Fantasy**  
     Le style fantasy est un style cartographique regroupant plus de concepts artistiques que cartographiques.   
     Le fantasy demeure cependant un style de cartographie. 
     Celui-ci est parfaitement adapté aux cartes figurant et agrémentant la lecture dans un ouvrage imaginaire, fantasy ou autre.     
     Laissez vous séduire par sa complexité et sa sobriété unique. Un style parfaitement modulable selon vos envies.     
     Formats compatibles: A5; A4; A3     
     Prix: entre 0.50 et 2 € + format     \n
+"""
+
+atlas= """---
 2) ##### **Atlas**    
     Ce style de cartographie particulier est une compilation d'une carte politique, physique, routière, démographique, etc.
     Contrairement à d'autres styles, une carte atlas sera fort lumineuse, vivement taborée avec des pigments choisis et gorgée d'informations (simplifiables ou pas selon vos choix).
     Son style de conception et son adaptabilité à tous les formats sont ses atouts majeurs.
     Laissez vous guider par ce style sans nul autre égal.       
     Format compatibles: A5; A4; A3; A2      
-    Prix entre: 0.75 et 5 € + format\n      
+    Prix entre: 0.75 et 5 € + format\n     
+
+"""    
+     
+minimalist = """---
 3) ##### **Minimalist**     
     Ce style permet de réduire à l'essentiel le complexe (tout l'inverse d'un Atlas, il sera plus nu, moins vif mais très compréhensible et aéré).
     Avec peu de traits et de tons, ce style permet d'identifier les forêts denses, les villes etc. Son charme est sa simplicité et sa façon de faire deviner la matière. Ses attributs permettent d'en faire un carte des plus maniables existantes : sa libre nomenclature, ses polices de texte associées, ses environnements,...      
     Formats compatibles: A5; A4; A3; A2     
     Prix: 0 €\n
+"""
+st.write(intro)
+st.write(fantasy)
+col1,col2,col3 = st.columns(3)
+with col1:
+    afficher_image("IMG_2393.jpg")
+with col2:
+    afficher_image("IMG_2387.jpg")
+with col3:
+    afficher_image("IMG_2392.jpg")
+st.write(atlas)
+col3,col1,col2,col4 = st.columns([1,2.5,2,1],gap="large",vertical_alignment="center")
+with col1:
+    afficher_image("IMG_2394.jpg")
+with col2:
+    afficher_image("IMG_2395.jpg")
+st.write(minimalist)
+col3,col1,col2,col4 = st.columns([1,2.5,2,1],gap="large",vertical_alignment="center")
+with col1:
+    afficher_image("IMG_2388.jpg")
+with col2 :
+    afficher_image("IMG_2391.jpg")
 
-### TARIFS\n
-""")
+st.write("---\n### TARIFS\n")
+
 df = {
         "format" : ["A5", "A4", "A3","A2"],
         "simple" : ["0,75 €", "1 €", "3 €","5,50 €"],
@@ -61,7 +98,7 @@ df = pd.DataFrame.from_dict(df, orient='index')
 df = df.transpose()
 st.dataframe(df, use_container_width=True,hide_index=True)
 grille_de_prix = {"A5":[0.75,0.50,0.50,0.25,0.50,0.50,0.25],"A4":[1,0.50,1,0.50,0.50,0.50,0.25],"A3":[3,1.50,2,1,0.50,0.50,0.25],"A2":[5.50,3,3.50,2.50,0.50,0.50,0.25]}
-st.write("##### **+ 0,25 € par heure de travail**")
+st.write("##### **+ 0,25 € par heure de travail**\n---")
 
 st.write("### SIMULATION")
 with st.form("simulation"):
@@ -107,8 +144,4 @@ with st.form("simulation"):
             resume_estimation = resume_estimation+" "+f"{element_princ} élément(s) principal(aux)"
         if element_sec != 0:
             st.write(f"    -{element_sec} élément(s) secondaire(s)")
-            resume_estimation = resume_estimation+" "+f"{element_sec} élément(s) secondaire(s)"
-        
-# image = Image.open("IMG_2387.jpg")
-# ,"IMG_2388.jpg","IMG_2391.jpg"
-# st.image(image)
+            resume_estimation = resume_estimation+" "+f"{element_sec} élément(s) secondaire(s)"   
